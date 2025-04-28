a6c181201e1a4375ea035be5becd5a1be51f548e
Anson Jia <ansonjia123@gmail.com>
2025-04-27 23:26:42 -0400

    updated app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
+import { useState } from 'react';
 import Card from './Card.tsx'
 import List, { ListProps } from './List.tsx'
 import Counter from './counter.tsx'
 import OrderInfo from './OrderInfo.tsx';
 import ColorPicker from './ColorPicker.tsx';
+import ToDoList from './toDoList.tsx';
+
 
 function App() {
-  let listItems = ["item 1", "item 2", "item 3", "item 4 ", "item 5"];
-  let items = ["New York","San Francisco","Tokyo", "London", "Paris"];
+  const [listItems, setlistItems] = useState(["item 1", "item 2", "item 3", "item 4 ", "item 5"]);
+  const [cities, setCities] = useState(["New York","San Francisco","Tokyo", "London", "Paris"]);
 
   return(
     <>
     <Card/>
     <List/>
-    <ListProps items={listItems} heading="Reusable list"/>
+    <ListProps items={listItems} set={setlistItems} heading="Reusable list"/>
     <br/>
-    <ListProps items={items} heading="Cities"/>
+    <ListProps items={cities} set={setCities} heading="Cities"/>
     <br/>
     <Counter/>
     <br/>
     <OrderInfo/>
     <br/>
     <ColorPicker/>
+    <br/>
+    <ToDoList/>
     </>
     
   );
